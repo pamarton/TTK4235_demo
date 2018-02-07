6e8e757cbd214aae92b07898b34a1f38937b1c5f
Amund Marton <pamarton@stud.ntnu.no>
2018-02-07 13:34:04 +0100

    NICE

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 int main(){
-	printf("Hello world!!\n");
-	printf("...and Mars!\n");
-	int i = 1;
-	for(i = 0; i < 100; i++){
-		printf("HAIL STATAN");
-	}
+	printf("Hello world\n");
+	printf("...and Jupiter\n");
 	if(1 > 0){
 		return 1;
 	}
-
 	return 0;
 }
